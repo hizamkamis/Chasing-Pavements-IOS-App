6c9507375898f4adba9117d8a9a0093f6d7b66e6
Muhd Hizam <daniel.hizam@gmail.com>
2014-10-04 13:16:31 +0800

    Created the simple UI. Also added the transition for new game and exit

---
+++ Chasing Pavements/GameScene.swift
@@ -0,0 +1,28 @@
+//
+//  GameScene.swift
+//  Chasing Pavements
+//
+//  Created by Muhd Hizam on 1/10/14.
+//  Copyright (c) 2014 PRIxDE Agency. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+      
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+        for touch: AnyObject in touches {
+       
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
